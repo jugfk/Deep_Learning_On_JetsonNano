5f867745903fd118fa48d19b32a18c547785e96c
Jetson User Group For Korean <59607996+jetsonworld@users.noreply.github.com>
2020-04-14 21:42:05 +0900

    Add files via upload

---
+++ 06_Gradient_Boosting/10_xgboost_for_Regression.py
@@ -0,0 +1,24 @@
+# xgboost for regression
+from numpy import asarray
+from numpy import mean
+from numpy import std
+from sklearn.datasets import make_regression
+from xgboost import XGBRegressor
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import RepeatedKFold
+from matplotlib import pyplot
+# define dataset
+X, y = make_regression(n_samples=1000, n_features=10, n_informative=5, random_state=1)
+# evaluate the model
+model = XGBRegressor(objective='reg:squarederror')
+cv = RepeatedKFold(n_splits=10, n_repeats=3, random_state=1)
+n_scores = cross_val_score(model, X, y, scoring='neg_mean_absolute_error', cv=cv, n_jobs=-1, error_score='raise')
+print('MAE: %.3f (%.3f)' % (mean(n_scores), std(n_scores)))
+# fit the model on the whole dataset
+model = XGBRegressor(objective='reg:squarederror')
+model.fit(X, y)
+# make a single prediction
+row = [2.02220122, 0.31563495, 0.82797464, -0.30620401, 0.16003707, -1.44411381, 0.87616892, -0.50446586, 0.23009474, 0.76201118]
+row = asarray(row).reshape((1, len(row)))
+yhat = model.predict(row)
+print('Prediction: %.3f' % yhat[0])
