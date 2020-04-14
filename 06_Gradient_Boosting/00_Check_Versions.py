4c01add7843523af7c9ae46797c8b9e4276515ae
Jetson User Group For Korean <59607996+jetsonworld@users.noreply.github.com>
2020-04-14 20:57:21 +0900

    Add files via upload

--- 06_Gradient_Boosting/00_Check_Versions.py
+++ 06_Gradient_Boosting/00_Check_Versions.py
@@ -1,6 +1,8 @@
 # check scikit-learn version
 import sklearn
 import lightgbm
+import xgboost
+
 print("===================================")
 print(" - Scikit_Learn: ")
 print(sklearn.__version__)
@@ -8,4 +10,7 @@ print("===================================")
 # check lightgbm version
 print(" - LightGBM: ")
 print(lightgbm.__version__)
+print("===================================")
+print(" - xgboost: ")
+print(xgboost.__version__)
 print("===================================")
